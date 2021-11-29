f8163ac9e3dbe11622555e09160c47c9a90d5b26
tonny29 <zannatulnayemtonny@gmail.com>
2021-11-29 23:52:50 +0600

    fifth commit

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutMe = () => {
     return (
         <div>
-            <h1>About me</h1>
+            <p>You are reading about Jannatul Nayem Tonny. I am Bs.c in 3rd year stusent. Department of Computer Science & Engineering(CSE) from World University of Bangladesh. Beside I am a member of Programming hero 4th batch. I would like to introduce myself as a junior web developer.Became a Successful web developer is my dream. I already learned Basic HTML, CSS, java script. Even I'm able to make any type of simple web template. I already build some simple website. I learn and practice every day to reach my skill next level.I started my web Journey from April 2021. But stoped myself again and again. I couldn't practice properly. I was very worrid about myself. But I was determind. That time I learned basic CSS and HTML. And made some simple website only using html and css.In june 2021 I already learned different types of html tag, css tag, And some java script. I knocked Jhanker Mahmud by email. He gaves me some important suggestions. I followed his Directions. I can made any type of simple website. Now am a Jounior web developer.</p>
         </div>
     );
 };
